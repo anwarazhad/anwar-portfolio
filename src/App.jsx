2f86defc77cb77a5f2de02790123e6fe6ff1c558
susshi <anwarazhad00@gmail.com>
2026-07-22 16:51:40 +0800

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import CyberBackground from "./components/CyberBackground";
 
