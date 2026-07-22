d054bb037f7a47fb0f543999afd7b4f2b1c0afc1
susshi <anwarazhad00@gmail.com>
2026-07-22 16:43:53 +0800

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import CyberBackground from "./components/CyberBackground";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
@@ -5,7 +6,6 @@ import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import CyberBackground from "./components/CyberBackground";
 
 function App() {
   return (
