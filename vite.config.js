140da70f931891e7bdba13f9e09e09bbcb237c0c
susshi <anwarazhad00@gmail.com>
2026-07-23 14:20:25 +0800

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  base: "/anwar-portfolio/",
+  base: "/anwar-portfolio/src/App.jsx/",
 })
